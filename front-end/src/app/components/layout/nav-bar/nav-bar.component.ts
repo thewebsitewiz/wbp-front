6aff5f00f025b7b9ff2d4a0fa9abcadcdb3c2b47
thewebsitewiz <dennis@thewebsitewiz.com>
2025-01-11 13:49:31 -0500

    before nav adjustmets

--- front-end/src/app/components/layout/nav-bar/nav-bar.component.ts
+++ front-end/src/app/components/layout/nav-bar/nav-bar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'wbp-nav-bar',
   standalone: true,
   imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss',
+  styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    if (window.scrollY > 50) {
+      navbar.classList.add(
+        'navbar-scrolled',
+        'border-bottom',
+        'border-secondary',
+        'navbar-sticky'
+      );
+    } else {
+      navbar.classList.remove(
+        'navbar-scrolled',
+        'border-bottom',
+        'border-secondary',
+        'navbar-sticky'
+      );
+    }
+    console.log('User scrolled');
+  }
+}
--- front-end/src/app/components/pages/gallery/gallery.component.spec.ts
+++ front-end/src/app/components/pages/gallery/gallery.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { GalleryComponent } from './gallery.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('GalleryComponent', () => {
+  let component: GalleryComponent;
+  let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
+      imports: [GalleryComponent]
     })
     .compileComponents();
-    
-    fixture = TestBed.createComponent(FooterComponent);
+
+    fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
